79a5fe0300ef6097c59b6e7a0970a97456421870
sorchatiernan <tiernaso@tcd.ie>
2020-02-11 16:36:16 +0000

    Attempted to write the mergeSortRecursive function

--- src/SortComparison.java
+++ src/SortComparison.java
@@ -112,11 +112,53 @@ class SortComparison {
 	 * @return after the method returns, the array must be in ascending sorted order.
 	 */
 	static double[] mergeSortRecursive (double a[]) {
-		return null;
+		double [] aux = new double [a.length];
+		double [] aSorted = sort(a,aux 0, a.length - 1);
 
 		//todo: implement the sort
 
 	}//end mergeSortRecursive
+	private static double [] sort(double [] a, double [] aux, int lo, int hi)
+	{
+		if (hi <= lo) 
+		{
+			return aux;
+		}
+		else
+		{
+			int mid = lo + (hi - lo)/2;
+			sort(a, aux, lo, mid);
+			sort(a, aux, mid+1, hi);
+			double [] aSorted = merge(a, aux, lo, mid, hi);
+			return aSorted;
+		}
+		
+	}
+	private static double [] merge(double [] a, double [] aux, int lo, int mid, int hi)
+	{
+		for (int k = lo; k <= hi; k++)
+		{
+			aux[k] = a [k];
+		}
+		int i = lo;
+		int j = mid+1;
+		for(int k = lo; k<= hi; k++)
+		{
+			if (i > mid)
+			{
+				a[k] = aux[j++];
+			}
+			else if (j > hi)
+			{
+				a[k] = aux[i++];
+			}
+			else if ((aux[j] < aux[i]))
+			{
+				a[k] = aux[i++];
+			}
+		}
+		return a;
+	}
 
 
 
