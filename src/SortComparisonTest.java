03544fcdc1d6a8097aacffa431d579a3728deea4
sorchatiernan <tiernaso@tcd.ie>
2020-02-11 16:19:05 +0000

    Wrote my tests for insertionSort and created a constant DELTA

--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -15,6 +16,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SortComparisonTest
 {
+	private static final double DELTA = 1e-15;
     //~ Constructor ........................................................
     @Test
     public void testConstructor()
@@ -44,6 +46,30 @@ public class SortComparisonTest
     // be executed at least once from at least one test.
 
     // ----------------------------------------------------------
+    @Test
+    public void testInsertionSort()
+    {
+    	//test an array with one element
+    	double [] singleArray = {1.5};	
+    	double [] singleArraySorted = SortComparison.insertionSort(singleArray);
+    	assertArrayEquals(singleArraySorted, singleArray, DELTA);
+
+    	//tests a positive array
+    	double [] positiveArray = {3.2, 8.9, 1.0, 11.4, 5.5};
+    	double [] positiveArraySorted = SortComparison.insertionSort(positiveArray);
+    	assertArrayEquals(positiveArray, positiveArraySorted, DELTA);
+
+    	//test a negative array
+    	double [] negativeArray = {-3.2, -8.9, -1.0, -11.4, -5.5};
+    	double [] negativeArraySorted = SortComparison.insertionSort(negativeArray);
+    	assertArrayEquals(negativeArray, negativeArraySorted, DELTA);
+
+    	//tests an array with both negative and positive components
+    	double [] mixedArray = {3.2, -8.9, 1.0, -11.4, -5.5};
+    	double [] mixedArraySorted = SortComparison.insertionSort(mixedArray);
+    	assertArrayEquals(mixedArray, mixedArraySorted, DELTA);
+
+    }
     /**
      *  Main Method.
      *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
