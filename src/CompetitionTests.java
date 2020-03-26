c78cd4389b4d23171691202d462c219f44f2b844
sorchatiernan <tiernaso@tcd.ie>
2020-03-26 10:05:35 +0000

    Set up project

---
+++ src/CompetitionTests.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class CompetitionTests {
+
+    @Test
+    public void testDijkstraConstructor() {
+
+        //TODO
+    }
+
+    @Test
+    public void testFWConstructor() {
+        //TODO
+    }
+
+    //TODO - more tests
+    
+}
