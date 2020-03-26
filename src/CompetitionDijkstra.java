9b3b66c4405e009caa1f8d2259ee57c675216f13
sorchatiernan <tiernaso@tcd.ie>
2020-03-26 10:43:27 +0000

    Wrote the constructor for CompetitionDijkstra

--- src/CompetitionDijkstra.java
+++ src/CompetitionDijkstra.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /*
  * A Contest to Meet (ACM) is a reality TV contest that sets three contestants at three random
  * city intersections. In order to win, the three contestants need all to meet at any intersection
@@ -17,13 +21,73 @@
 
 public class CompetitionDijkstra {
 
+	public String filename;
+	public int speedA;
+	public int speedB;
+	public int speedC;
+	public double distanceToNextIntersection [][];
+	public int edgeToIntersection [][];
+	public int totalEdges;
+	public int totalNumberOfIntersections;
+	public int totalNumberOfStreets;
     /**
      * @param filename: A filename containing the details of the city road network
      * @param sA, sB, sC: speeds for 3 contestants
     */
     CompetitionDijkstra (String filename, int sA, int sB, int sC){
-
-       //TODO
+    	this.speedA = sA;
+    	this.speedB = sB;
+    	this.speedC = sC;
+    	
+    	try
+    	{
+    		File file = new File(filename);
+    		Scanner s = new Scanner(file);
+    		int i = 0;
+    		while(s.hasNextLine())
+    		{
+    			String [] currentLine = s.nextLine().trim().split("\\s+");
+    			if ( i ==1);
+    			{
+    				totalEdges = Integer.parseInt(currentLine[i-1]);
+    			}
+    			if (i ==0)
+    			{
+    				distanceToNextIntersection = new double[Integer.parseInt(currentLine[i])][Integer.parseInt(currentLine[i])];
+    				edgeToIntersection = new int[Integer.parseInt(currentLine[i])][Integer.parseInt(currentLine[i])];
+    				for(int j = 0; j < distanceToNextIntersection.length; j++)
+    				{
+    					for(int h = 0; h < distanceToNextIntersection[j].length; h ++)
+    					{
+    						distanceToNextIntersection[j][h] = Integer.MAX_VALUE;
+    						if(j ==h)
+    						{
+    							distanceToNextIntersection[j][h] = 0;
+    						}
+    					}
+    				}
+    			}
+    			
+    			else 
+    			{
+    				distanceToNextIntersection[Integer.parseInt(currentLine[0])][Integer.parseInt(currentLine[1])] = Double.parseDouble(currentLine[2]);
+    				edgeToIntersection[Integer.parseInt(currentLine[0])][Integer.parseInt(currentLine[1])] = Integer.parseInt(currentLine[0]);
+    			}
+    			i++;
+    			
+    			
+    		}
+    		for(int k = 0; k <distanceToNextIntersection.length; k++)
+    		{
+    			findShortestPath(k);
+    		}
+    		s.close();	
+    	}
+    	catch (Exception x) {
+    		distanceToNextIntersection = new double[0][0];
+    		edgeToIntersection = new int[0][0];
+    		return;
+    		}
     }
 
 
@@ -34,6 +98,10 @@ public class CompetitionDijkstra {
 
         //TO DO
         return -1;
+    }
+    public void findShortestPath(int i)
+    {
+    	
     }
 
 }
